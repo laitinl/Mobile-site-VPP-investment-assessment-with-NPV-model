5caebaa47c805f2997868c637277f19b16ca7844
Leo Laitinen <leo.laitinen1@gmail.com>
2026-09-16 09:33:18 +0300

    Add new PERT sampled parameters and fix wrong equations

--- src/npv_simulator.py
+++ src/npv_simulator.py
@@ -23,21 +23,23 @@ class NPVSimulator:
         self.config = config
 
     def _calculate_npv(
-        self, cash_flows: np.ndarray, discount_rate: float
+        self, cash_flows: np.ndarray, discount_rate: np.array
     ) -> np.ndarray:
         """
         Calculate the Net Present Value (NPV) for given cash flows and discount rate.
 
         Args:
         cash_flows (np.ndarray): Array of cash flows.
-        discount_rate (float): Discount rate for NPV calculation.
+        discount_rate (np.array): Discount rates for NPV calculation.
 
         Returns:
         np.ndarray: NPV values.
         """
         years = np.arange(0, self.config["n_years"] + 1)
         cash_flows = (
-            cash_flows * (1 + discount_rate) ** -years[np.newaxis, :, np.newaxis]
+            cash_flows
+            * (1 + discount_rate[:, np.newaxis, np.newaxis])
+            ** -years[np.newaxis, :, np.newaxis]
         )
         npvs = np.cumsum(cash_flows, axis=1)
         return npvs
@@ -55,13 +57,11 @@ class NPVSimulator:
         """
         np.random.seed(random_seed)
 
-        # Fixed parameters
+        # Constant parameters
         n_years = self.config["n_years"]
-        battery_capacity_cost = self.config["battery_capacity_cost"]
         fixed_battery_installation_cost, variable_battery_installation_cost = (
             self.config["battery_installation_cost"]
         )
-        vpp_controller_cost = self.config["vpp_controller_cost"]
         ffr_yield = self.config["ffr_yield"]
         fcr_yield = self.config["fcr_yield"]
         afrr_yield = self.config["afrr_yield"]
@@ -69,34 +69,15 @@ class NPVSimulator:
         peak_shaving_savings_per_site = self.config["peak_shaving_savings_per_site"]
         site_mean_power = self.config["site_mean_power"]
         vpp_total_power = self.config["vpp_total_power"]
-        discount_rate = self.config["discount_rate"]
-        connectivity_cost = self.config["connectivity_cost"]
-
-        # Derived parameters
-        n_sites = vpp_total_power // site_mean_power
-        battery_capacity = (
-            self.cases["Investment_size"] * vpp_total_power
-        )  # Assuming battery capacity equals VPP total power for one hour
-        investment_cost = (
-            battery_capacity * battery_capacity_cost
-            + n_sites
-            * fixed_battery_installation_cost
-            * np.array([[np.abs(np.sign(x)) for x in self.cases["Investment_size"]]])
-            + battery_capacity * variable_battery_installation_cost
-            + n_sites * vpp_controller_cost * self.cases["Controller"]
-        )
-        om_cost = (
-            battery_capacity * battery_capacity_cost
-            + n_sites * vpp_controller_cost * self.cases["Controller"]
-        ) * self.config["o&m_cost"]
-        annual_cost = om_cost + connectivity_cost * n_sites * self.cases["Connectivity"]
-        reserve_market_yield = (
-            self.cases["FFR weight"] * ffr_yield
-            + self.cases["FCR weight"] * fcr_yield
-            + self.cases["aFRR weight"] * afrr_yield
-        )
 
         # Sampled parameters
+        battery_capacity_cost = pert(*self.config["battery_capacity_cost"]).rvs(
+            size=count
+        )
+        vpp_controller_cost = pert(*self.config["vpp_controller_cost"]).rvs(size=count)
+        discount_rate = pert(*self.config["discount_rate"]).rvs(size=count)
+        connectivity_cost = pert(*self.config["connectivity_cost"]).rvs(size=count)
+        om_cost = pert(*self.config["o&m_cost"]).rvs(size=count)
         reserve_price_multiplier = pert(
             *self.config["reserve_price_multiplier_dist"]
         ).rvs(size=count)
@@ -108,10 +89,42 @@ class NPVSimulator:
         ).rvs(size=count)
         bsp_fee = pert(*self.config["bsp_fee_dist"]).rvs(size=count)
 
+        # Derived parameters
+        n_sites = np.ceil(vpp_total_power / site_mean_power).astype(int)
+        battery_capacity = (
+            self.cases["Investment_size"] * vpp_total_power
+        )  # Assuming battery capacity equals VPP total power for one hour
+        investment_cost = (
+            battery_capacity[np.newaxis, :] * battery_capacity_cost[:, np.newaxis]
+            + n_sites
+            * fixed_battery_installation_cost
+            * np.array([np.abs(np.sign(x)) for x in self.cases["Investment_size"]])[
+                np.newaxis, :
+            ]
+            + battery_capacity[np.newaxis, :] * variable_battery_installation_cost
+            + n_sites * vpp_controller_cost[:, np.newaxis] * self.cases["Controller"]
+        )
+        om_expense = (
+            battery_capacity[np.newaxis, :] * battery_capacity_cost[:, np.newaxis]
+            + n_sites * vpp_controller_cost[:, np.newaxis] * self.cases["Controller"]
+        ) * om_cost[:, np.newaxis]
+        annual_cost = (
+            om_expense
+            + connectivity_cost[:, np.newaxis]
+            * n_sites
+            * self.cases["Connectivity"][np.newaxis, :]
+        )
+        reserve_market_yield = (
+            self.cases["FFR weight"] * ffr_yield
+            + self.cases["FCR weight"] * fcr_yield
+            + self.cases["aFRR weight"] * afrr_yield
+        )
+
         # Calculate cash flows
         cash_flows = np.zeros(
             (count, n_years + 1, self.cases["Investment_size"].shape[0])
         )
+        revs = np.zeros((count, n_years + 1, self.cases["Investment_size"].shape[0]))
         cash_flows[:, 0, :] = -investment_cost
 
         for year in range(1, n_years + 1):
@@ -135,13 +148,19 @@ class NPVSimulator:
                 * self.cases["Peak shaving"][np.newaxis, :]
             )
             cash_flows[:, year, :] = (
+                (reserve_market_revenue) * (1 - bsp_fee[:, np.newaxis])
+                + load_shifting_revenue
+                + peak_shaving_revenue
+                - annual_cost[np.newaxis, :]
+            )
+            revs[:, year, :] = (
                 reserve_market_revenue + load_shifting_revenue + peak_shaving_revenue
-            ) * (1 - bsp_fee[:, np.newaxis]) - annual_cost[np.newaxis, :]
+            )
 
         # Calculate NPV
         out = self._calculate_npv(cash_flows, discount_rate)
 
-        return out
+        return out, np.median(cash_flows, axis=0), np.median(revs, axis=0)
 
     def _calculate_npv_sensitivity(self, df):
         # Fixed parameters
@@ -217,8 +236,11 @@ class NPVSimulator:
                 * self.cases["Peak shaving"][np.newaxis, :]
             )
             cash_flows[:, year, :] = (
-                reserve_market_revenue + load_shifting_revenue + peak_shaving_revenue
-            ) * (1 - bsp_fee) - annual_cost[np.newaxis, :]
+                reserve_market_revenue * (1 - bsp_fee)
+                + load_shifting_revenue
+                + peak_shaving_revenue
+                - annual_cost[np.newaxis, :]
+            )
 
         # Calculate NPV
         out = self._calculate_npv(cash_flows, discount_rate)
@@ -245,20 +267,20 @@ class NPVSimulator:
             "power_charge_multiplier_dist",
         ]
         param_values = [
-            self.config["battery_capacity_cost"],
+            self.config["battery_capacity_cost"][1],
             self.config["battery_installation_cost"][0],
             self.config["battery_installation_cost"][1],
-            self.config["vpp_controller_cost"],
+            self.config["vpp_controller_cost"][1],
             self.config["ffr_yield"],
             self.config["fcr_yield"],
             self.config["afrr_yield"],
             self.config["load_shifting_savings"],
             self.config["peak_shaving_savings_per_site"],
-            self.config["connectivity_cost"],
-            self.config["o&m_cost"],
+            self.config["connectivity_cost"][1],
+            self.config["o&m_cost"][1],
             self.config["bsp_fee_dist"][1],
             self.config["site_mean_power"],
-            self.config["discount_rate"],
+            self.config["discount_rate"][1],
             self.config["reserve_price_multiplier_dist"][1],
             self.config["spot_volatility_multiplier_dist"][1],
             self.config["power_charge_multiplier_dist"][1],
