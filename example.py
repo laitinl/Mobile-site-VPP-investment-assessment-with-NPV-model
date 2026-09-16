982af87102af97510a903014594ea1dc7ffd84f1
Leo Laitinen <leo.laitinen1@gmail.com>
2026-09-16 09:35:33 +0300

    Add new tornado plots to the main function. Add analysis function for investigating the simulation outputs

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 # %%
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from src.npv_simulator import NPVSimulator
 from src.utils.plotting import (
@@ -8,19 +7,21 @@ from src.utils.plotting import (
     plot_npv_hist_grid,
     plot_npv_boxplots,
     plot_npv_hist_overlaid,
+    plot_tornado,
+    plot_tornado_grid,
 )
 
 
 def main():
     cases = {
         "Investment_size": np.array(
-            [0, 1, 4, 0, 1, 4, 0, 1, 4]
+            [0, 1, 3, 0, 1, 3, 0, 1, 3]
         ),  # Extra capacity hours
         "FFR weight": np.array([0, 0, 0, 1, 0, 0, 1, 0, 0]),  # Weight of FFR revenues
         "FCR weight": np.array([0, 0, 0, 0, 1, 0, 0, 1, 0]),  # Weight of FCR revenues
         "aFRR weight": np.array([0, 0, 0, 0, 0, 1, 0, 0, 1]),  # Weight of aFRR revenues
         "LS weight": np.array(
-            [0, 0.25, 1, 0, 0, 0, 0, 0, 0]
+            [0, 0.33, 1, 0, 0.33, 1, 0, 0.33, 1]
         ),  # Weight of load shifting revenues
         "Controller": np.array(
             [False, False, False, False, False, False, True, True, True]
@@ -29,26 +30,26 @@ def main():
             [False, True, True, True, True, True, True, True, True]
         ),  # Whether VPP connectivity is used
         "Peak shaving": np.array(
-            [False, True, True, False, True, True, True, True, True]
+            [False, True, True, False, True, True, False, True, True]
         ),  # Whether peak shaving is used
     }
 
     config = {
         "n_years": 10,
-        "battery_capacity_cost": 100,  # Cost per kWh
+        "battery_capacity_cost": (50, 115, 200),  # Cost per kWh
         "battery_installation_cost": (
             500,
             25,
-        ),  # Installation cost per site (fixed cost, cost per kWh)
-        "vpp_controller_cost": 1500,  # Cost for VPP controller per
-        "ffr_yield": 23561,  # Yield from FFR up and down market €/MW/year
-        "fcr_yield": 109000 + 118000,  # Yield from FCR-D up and down market €/MW/year
-        "afrr_yield": 176000 + 141000,  # Yield from aFRR up and down market €/MW/year
-        "load_shifting_savings": (40 * 4) * 365,  # Savings from load shifting €/MW/year
+        ),  # Installation cost per site (fixed cost, variable cost per kWh)
+        "vpp_controller_cost": (500, 1500, 2500),  # Cost for VPP controller per
+        "ffr_yield": 11840,  # Yield from FFR up and down market €/MW/year
+        "fcr_yield": 119890,  # Yield from FCR-D up and down market €/MW/year
+        "afrr_yield": 131354,  # Yield from aFRR up and down market €/MW/year
+        "load_shifting_savings": 67882,  # Savings from load shifting €/MW/year
         "peak_shaving_savings_per_site": (1.35 * 2)
         * 12,  # Single site savings from peak shaving €/MW/year
-        "connectivity_cost": 240,  # VPP connectivity cost per year
-        "o&m_cost": 0.02,  # O&M cost as a fraction of investment cost
+        "connectivity_cost": (0, 12, 120),  # VPP connectivity cost per year
+        "o&m_cost": (0.01, 0.02, 0.03),  # O&M cost as a fraction of investment cost
         "bsp_fee_dist": (
             0.1,
             0.2,
@@ -56,7 +57,7 @@ def main():
         ),  # BSP fee distribution parameters (min, mode, max)
         "site_mean_power": 2.5,  # Mean power per site in kW
         "vpp_total_power": 1000,  # Total power of the VPP in kW
-        "discount_rate": 0.057,  # Discount rate for NPV calculation
+        "discount_rate": (0.04, 0.057, 0.08),  # Discount rate for NPV calculation
         "reserve_price_multiplier_dist": (
             -0.5,
             -0.2,
@@ -74,45 +75,159 @@ def main():
         ),  # Power charge multiplier distribution (min, mode, max)
     }
 
-    def make_tornado_plot(sens_params, sens_array, scenario, title=None):
-        df_sensitivity = pd.DataFrame(
-            {
-                "param_names": sens_params,
-                "sens_plus": sens_array[:, 0, scenario],
-                "sens_minus": sens_array[:, 1, scenario],
-            }
-        )
-        df_sensitivity["delta"] = np.abs(
-            df_sensitivity["sens_plus"] - df_sensitivity["sens_minus"]
-        )
-        df_sensitivity.sort_values("delta", inplace=True)
-        plt.grid(axis="y", alpha=0.5)
-        plt.barh(
-            np.arange(len(df_sensitivity)),
-            df_sensitivity["sens_plus"],
-            label="Parameter increase 20%",
-        )
-        plt.barh(
-            np.arange(len(df_sensitivity)),
-            df_sensitivity["sens_minus"],
-            label="Parameter decrease 20%",
-        )
-        plt.yticks(np.arange(len(df_sensitivity)), df_sensitivity["param_names"])
-        plt.legend()
-        plt.title(title)
-        plt.xlabel("NPV change (€)")
-        plt.show()
+    simulator = NPVSimulator(cases, config)
+    sens_params, sens_array = simulator.run_sensitivity_analysis()
+    scenarios = ["1a", "1b", "1c", "2a", "2b", "2c", "3a", "3b", "3c"]
+    for i in range(len(scenarios)):
+        plot_tornado(sens_params, sens_array, i, title=f"Scenario {scenarios[i]}")
+    plot_tornado_grid(sens_params, sens_array, scenarios)
+
+    results_with_different_site_power = []
+    results, cf, revs = simulator.run_uncertainty_analysis(count=3000000)
+    results_with_different_site_power.append(results)
+
+    # Boxplot for different site mean power scenarios
+    config["site_mean_power"] = 5
+    simulator = NPVSimulator(cases, config)
+    results, cf, revs = simulator.run_uncertainty_analysis(count=3000000)
+    results_with_different_site_power.append(results)
+    for i in range(len(scenarios)):
+        npv_last_year = results[:, -1, i]
+        print(f"Scenario {scenarios[i]} with site mean power 5 kW:")
+        print(f"Median NPV: {np.median(npv_last_year)}\n")
+
+    scenario = 4
+    npv_last_year = results[:, -1, scenario]
+    print(f"Median NPV: {np.median(npv_last_year)}")
+    # print(f"Standard Deviation of NPV: {np.std(npv_last_year)}")
+    print(f"Minimum NPV: {np.min(npv_last_year)}")
+    print(f"Maximum NPV: {np.max(npv_last_year)}")
+    print(f"5th Percentile NPV: {np.percentile(npv_last_year, 5)}")
+    print(f"95th Percentile NPV: {np.percentile(npv_last_year, 95)}")
+    print(cf[:, scenario])
+
+    plt.figure(figsize=(10, 6))
+    plt.hist(npv_last_year, bins=500, density=True)
+    plt.xlabel("NPV (€)")
+    plt.ylabel("Density")
+    plt.show()
+
+    plot_cumulative_npv(results, scenario, config["n_years"])
+
+    config["site_mean_power"] = 7.5
+    simulator = NPVSimulator(cases, config)
+    results, cf, revs = simulator.run_uncertainty_analysis(count=3000000)
+    results_with_different_site_power.append(results)
+
+    config["site_mean_power"] = 15
+    simulator = NPVSimulator(cases, config)
+    results, cf, revs = simulator.run_uncertainty_analysis(count=3000000)
+    results_with_different_site_power.append(results)
+
+    plot_npv_boxplots(
+        site_powers=[2.5, 5, 7.5, 15],
+        results_with_different_site_power=results_with_different_site_power,
+        scenarios=scenarios,
+    )
+
+    plot_npv_hist_grid(
+        site_powers=[2.5, 5, 7.5, 15],
+        results_with_different_site_power=results_with_different_site_power,
+        scenarios=scenarios,
+    )
+
+    plot_npv_hist_overlaid(
+        site_powers=[2.5, 5, 7.5, 15],
+        results_with_different_site_power=results_with_different_site_power,
+        scenarios=scenarios,
+    )
+
+
+def analysis():
+    cases = {
+        "Investment_size": np.array(
+            [0, 1, 3, 0, 1, 3, 0, 1, 3]
+        ),  # Extra capacity hours
+        "FFR weight": np.array([0, 0, 0, 1, 0, 0, 1, 0, 0]),  # Weight of FFR revenues
+        "FCR weight": np.array([0, 0, 0, 0, 1, 0, 0, 1, 0]),  # Weight of FCR revenues
+        "aFRR weight": np.array([0, 0, 0, 0, 0, 1, 0, 0, 1]),  # Weight of aFRR revenues
+        "LS weight": np.array(
+            [0, 0.33, 1, 0, 0.33, 1, 0, 0.33, 1]
+        ),  # Weight of load shifting revenues
+        "Controller": np.array(
+            [False, False, False, False, False, False, True, True, True]
+        ),  # Whether a VPP controller is used
+        "Connectivity": np.array(
+            [False, True, True, True, True, True, True, True, True]
+        ),  # Whether VPP connectivity is used
+        "Peak shaving": np.array(
+            [False, True, True, False, True, True, False, True, True]
+        ),  # Whether peak shaving is used
+    }
+
+    config = {
+        "n_years": 10,
+        "battery_capacity_cost": (50, 115, 200),  # Cost per kWh
+        "battery_installation_cost": (
+            500,
+            25,
+        ),  # Installation cost per site (fixed cost, variable cost per kWh)
+        "vpp_controller_cost": (500, 1500, 2500),  # Cost for VPP controller per
+        "ffr_yield": 11840,  # Yield from FFR up and down market €/MW/year
+        "fcr_yield": 119890,  # Yield from FCR-D up and down market €/MW/year
+        "afrr_yield": 131354,  # Yield from aFRR up and down market €/MW/year
+        "load_shifting_savings": 67882,  # Savings from load shifting €/MW/year
+        "peak_shaving_savings_per_site": 24000
+        / 200,  # (1.35 *  2) * 12,  # Single site savings from peak shaving €/MW/year
+        "connectivity_cost": (0, 12, 120),  # VPP connectivity cost per year
+        "o&m_cost": (0.01, 0.02, 0.03),  # O&M cost as a fraction of investment cost
+        "bsp_fee_dist": (
+            0.1,
+            0.2,
+            0.3,
+        ),  # BSP fee distribution parameters (min, mode, max)
+        "site_mean_power": 2.5,  # Mean power per site in kW
+        "vpp_total_power": 1000,  # Total power of the VPP in kW
+        "discount_rate": (0.04, 0.057, 0.08),  # Discount rate for NPV calculation
+        "reserve_price_multiplier_dist": (
+            -0.5,
+            -0.2,
+            0.5,
+        ),  # Reserve price multiplier distribution (min, mode, max)
+        "spot_volatility_multiplier_dist": (
+            -0.0001,
+            0.0,
+            0.0001,
+        ),  # (-0.1, 0, 0.1),  # Spot price multiplier distribution (min, mode, max)
+        "power_charge_multiplier_dist": (
+            -0.0001,
+            0,
+            0.0001,
+        ),  # (-0.1, 0.2, 0.96),  # Power charge multiplier distribution (min, mode, max)
+    }
 
     simulator = NPVSimulator(cases, config)
     sens_params, sens_array = simulator.run_sensitivity_analysis()
     scenarios = ["1a", "1b", "1c", "2a", "2b", "2c", "3a", "3b", "3c"]
     for i in range(len(scenarios)):
-        make_tornado_plot(sens_params, sens_array, i, title=f"Scenario {scenarios[i]}")
+        plot_tornado(sens_params, sens_array, i, title=f"Scenario {scenarios[i]}")
+    plot_tornado_grid(sens_params, sens_array, scenarios)
 
     results_with_different_site_power = []
-    results = simulator.run_uncertainty_analysis(count=3000000)
+    results, cf, revs = simulator.run_uncertainty_analysis(count=3000000)
     results_with_different_site_power.append(results)
-    scenario = 2
+
+    # Boxplot for different site mean power scenarios
+    config["site_mean_power"] = 5
+    simulator = NPVSimulator(cases, config)
+    results, cf, revs = simulator.run_uncertainty_analysis(count=3000000)
+    results_with_different_site_power.append(results)
+    for i in range(len(scenarios)):
+        npv_last_year = results[:, -1, i]
+        print(f"Scenario {scenarios[i]} with site mean power 5 kW:")
+        print(f"Median NPV: {np.median(npv_last_year)}\n")
+
+    scenario = 4
     npv_last_year = results[:, -1, scenario]
     print(f"Median NPV: {np.median(npv_last_year)}")
     # print(f"Standard Deviation of NPV: {np.std(npv_last_year)}")
@@ -128,40 +243,11 @@ def main():
     plt.show()
 
     plot_cumulative_npv(results, scenario, config["n_years"])
-
-    # Boxplot for different site mean power scenarios
-    config["site_mean_power"] = 5
-    simulator = NPVSimulator(cases, config)
-    results = simulator.run_uncertainty_analysis(count=3000000)
-    results_with_different_site_power.append(results)
-
-    config["site_mean_power"] = 10
-    simulator = NPVSimulator(cases, config)
-    results = simulator.run_uncertainty_analysis(count=3000000)
-    results_with_different_site_power.append(results)
-
-    config["site_mean_power"] = 20
-    simulator = NPVSimulator(cases, config)
-    results = simulator.run_uncertainty_analysis(count=3000000)
-    results_with_different_site_power.append(results)
-
-    plot_npv_boxplots(
-        site_powers=[2.5, 5, 10, 20],
-        results_with_different_site_power=results_with_different_site_power,
-        scenarios=scenarios,
-    )
-
-    plot_npv_hist_grid(
-        site_powers=[2.5, 5, 10, 20],
-        results_with_different_site_power=results_with_different_site_power,
-        scenarios=scenarios,
-    )
-
-    plot_npv_hist_overlaid(
-        site_powers=[2.5, 5, 10, 20],
-        results_with_different_site_power=results_with_different_site_power,
-        scenarios=scenarios,
-    )
+    for scenario in range(len(scenarios)):
+        print(f"Revenues {scenarios[scenario]}:")
+        print(revs[:, scenario])
+        print(f"Cash Flows: {cf[:, scenario]}")
+        print("\n")
 
 
 if __name__ == "__main__":
